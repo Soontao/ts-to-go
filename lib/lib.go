730a6dd0adc23a9c0372a9cb34dc618c18a37356
Theo Sun <theo.sun.github@outlook.com>
2023-09-13 14:07:36 +0800

    chore: update

--- lib/lib.go
+++ lib/lib.go
@@ -1,5 +1,8 @@
 package lib
 
-func Value() string {
-	return "hello golang"
+import "github.com/antlr4-go/antlr/v4"
+
+func Parse(code string) IProgramContext {
+	p := NewTypeScriptParser(antlr.NewCommonTokenStream(NewTypeScriptLexer(antlr.NewInputStream("const a: number = 1")), antlr.TokenDefaultChannel))
+	return p.Program()
 }
--- lib/lib_test.go
+++ lib/lib_test.go
@@ -6,22 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValue(t *testing.T) {
+func TestSimpleParse(t *testing.T) {
 	assert := assert.New(t)
-	tests := []struct {
-		name string
-		want string
-	}{
-		{
-			"basic test",
-			"hello golang",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Value(); got != tt.want {
-				assert.Equal(tt.want, got)
-			}
-		})
-	}
+	program := Parse("const a: number = 1")
+	assert.NotNil(program)
+	program.Accept(BaseTypeScriptParserVisitor{})
 }
